78c7011757294eb493f01d02801358ef70a5187d
DanielaVilla <danielavilla2898@gmail.com>
2023-04-16 22:32:02 -0600

    Send email using fastapi-mail

--- app/presentation/routers/product.py
+++ app/presentation/routers/product.py
@@ -1,5 +1,8 @@
 from typing import List
 from fastapi import APIRouter, Depends, HTTPException, status
+from fastapi_mail import FastMail, MessageSchema,ConnectionConfig
+from starlette.responses import JSONResponse
+from typing import List
 
 from app.domain.product import (
     SkuAlreadyExistError,
@@ -13,7 +16,8 @@ from app.presentation.schema.product.product_errors import (
 from app.interface_adapters.session.session_manager import (
     product_query_usecase,
     product_command_usecase,
-    product_consult_command_usecase
+    product_consult_command_usecase,
+    user_query_usecase    
 )
 
 from app.usecase.product import (
@@ -28,8 +32,45 @@ from app.usecase.product_consult import (
     ProductConsultCommandUsecase
 )
 
+from app.usecase.user import (
+    UserQueryUsecase,
+    UserReadModel
+)
+
 router = APIRouter()
 
+# Send email
+
+async def send_mail(message, user_query_usecase):
+
+    conf = ConnectionConfig(
+        MAIL_USERNAME="fasttest2023api@outlook.com",
+        MAIL_FROM="fasttest2023api@outlook.com",
+        MAIL_PASSWORD="mailing2023",
+        MAIL_PORT=587,
+        MAIL_SERVER="smtp.office365.com",
+        MAIL_TLS=True,
+        MAIL_SSL=False
+        )
+        
+    users = user_query_usecase.get_users()
+    emails = []
+
+    for user in users:
+        emails.append(user.email)
+
+    message = MessageSchema(
+        subject="Product",
+        recipients=emails,  # List of recipients, as many as you can pass
+        body=message,
+        subtype="html"
+        )
+ 
+    fm = FastMail(conf)
+    await fm.send_message(message)
+    print(message)
+ 
+    return JSONResponse(status_code=200, content={"message": "email has been sent"})
 
 @router.post(
     "/create",
@@ -44,9 +85,12 @@ router = APIRouter()
 async def create_product(
     data: ProductCreateModel,
     product_command_usecase: ProductCommandUsecase = Depends(product_command_usecase),
+    user_query_usecase: UserQueryUsecase = Depends(user_query_usecase),
 ):
     try:
         product = product_command_usecase.create_product(data)
+
+        await send_mail("New product added to the catalog", user_query_usecase)
     except SkuAlreadyExistError as e:
         raise HTTPException(
             status_code=status.HTTP_409_CONFLICT,
@@ -75,11 +119,14 @@ async def update_product(
     product_id: str,
     data: ProductUpdateModel,
     product_command_usecase: ProductCommandUsecase = Depends(product_command_usecase),
+    user_query_usecase: UserQueryUsecase = Depends(user_query_usecase),
 ):
     try:
         updated_product = product_command_usecase.update_product(
             product_id, data
         )
+
+        await send_mail(f"{updated_product.name} has been updated", user_query_usecase)
     except ProductNotFoundError as e:
         raise HTTPException(
             status_code=status.HTTP_404_NOT_FOUND,
@@ -110,9 +157,12 @@ async def update_product(
 async def delete_product(
     product_id: str,
     product_command_usecase: ProductCommandUsecase = Depends(product_command_usecase),
+    user_query_usecase: UserQueryUsecase = Depends(user_query_usecase),
 ):
     try:
         product_command_usecase.delete_product_by_id(product_id)
+        
+        await send_mail(f"Product with ID {product_id} has been deleted", user_query_usecase)
 
     except ProductNotFoundError as e:
         raise HTTPException(
